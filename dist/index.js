67f07105ac6fe360fd391f9aa14bd9ca71e7db41
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2023-09-01 10:40:04 +0700

    update

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const routes_1 = __importDefault(require("./routes"));
+require("dotenv/config");
+const app = (0, express_1.default)();
+const PORT = process.env.PORT;
+app.use(express_1.default.json());
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)());
+app.use('/api/v1', routes_1.default);
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
