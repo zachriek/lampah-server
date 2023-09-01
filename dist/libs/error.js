99c9e4acbbc21511b339bd850b147521de115188
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2023-09-01 10:36:47 +0700

    commit

--- dist/libs/error.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleError = void 0;
-const handleError = (res, message, statusCode = 400) => {
-    return res.status(statusCode).json({ error: message });
-};
-exports.handleError = handleError;
